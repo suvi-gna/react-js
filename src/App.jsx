454b688b764b97505506e26ed546598aedd07870
suvigna <chinnam.suvigna@gmail.com>
2026-04-29 09:54:33 +0530

    featu:added about,projects,skills,contact

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,26 @@
 // import viteLogo from './assets/vite.svg'
 // import heroImg from './assets/hero.png'
 // import './App.css'
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
+import Skills from './components/Skills'
+import Projects from './components/Projects'
 
 function App() {
 
 
   return (
+    <>
     <h1>hello react</h1>
+    <h2>bye</h2>
+    <Home />
+    <About />
+    <Contact />
+    <Projects />
+    <Skills />
+    </>
+    
   )
 }
 
