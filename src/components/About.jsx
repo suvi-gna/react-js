8960baea9d84a74a06efbbdd7fd06565debf3216
suvigna <chinnam.suvigna@gmail.com>
2026-05-02 09:36:21 +0530

    useEffect

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,18 @@
+import { useEffect, useState } from "react"
+
 export default function About()
 {
+    const [count,setCount] = useState(0)
+
+    useEffect(() =>
+    {
+        console.log("count changed");
+        
+    },[count])
     return(
-        <h1>About page</h1>
+        <>
+            <h2>{count}</h2>
+            <button onClick={() => setCount(prevs => prevs+1)}>inc</button>
+        </>
     )
 }
